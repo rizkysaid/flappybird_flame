a46ff46ac2ab7f268dd19b3c3d4fc0e4320bcd66
rizkysaid <rizkysaid40@gmail.com>
2024-01-03 12:57:46 +0700

    game loop

--- lib/components/pipe_group.dart
+++ lib/components/pipe_group.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flame/components.dart';
 import 'package:flappy_bird/components/pipe.dart';
 import 'package:flappy_bird/game/configuration.dart';
@@ -7,13 +9,22 @@ import 'package:flappy_bird/game/pipe_position.dart';
 class PipeGroup extends PositionComponent with HasGameRef<FlappyBirdGame> {
   PipeGroup();
 
+  final _random = Random();
+
   @override
   Future<void> onLoad() async {
     position.x = gameRef.size.x;
 
+    final heightMinusGround = gameRef.size.y - Config.groundHeight;
+    final spacing = 100 + _random.nextDouble() * (heightMinusGround / 4);
+    final centerY =
+        spacing + _random.nextDouble() * (heightMinusGround - spacing);
+
     addAll([
-      Pipe(pipePosition: PipePosition.top, height: 100),
-      Pipe(pipePosition: PipePosition.bottom, height: 200),
+      Pipe(pipePosition: PipePosition.top, height: centerY - spacing / 2),
+      Pipe(
+          pipePosition: PipePosition.bottom,
+          height: heightMinusGround - (centerY + spacing / 2)),
     ]);
   }
 
@@ -21,5 +32,9 @@ class PipeGroup extends PositionComponent with HasGameRef<FlappyBirdGame> {
   void update(double dt) {
     super.update(dt);
     position.x -= Config.gameSpeed * dt;
+
+    if (position.x < -10) {
+      removeFromParent();
+    }
   }
 }
--- lib/game/flappy_bird_game.dart
+++ lib/game/flappy_bird_game.dart
@@ -1,18 +1,31 @@
 import 'package:flame/game.dart';
+import 'package:flame/timer.dart';
 import 'package:flappy_bird/components/background.dart';
 import 'package:flappy_bird/components/bird.dart';
 import 'package:flappy_bird/components/ground.dart';
 import 'package:flappy_bird/components/pipe_group.dart';
+import 'package:flappy_bird/game/configuration.dart';
 
 class FlappyBirdGame extends FlameGame {
+  FlappyBirdGame();
+
   late Bird bird;
+  Timer interval = Timer(Config.pipeInterval, repeat: true);
+
   @override
   Future<void> onLoad() async {
     addAll([
       Background(),
       Ground(),
       bird = Bird(),
-      PipeGroup(),
     ]);
+
+    interval.onTick = () => add(PipeGroup());
+  }
+
+  @override
+  void update(double dt) {
+    super.update(dt);
+    interval.update(dt);
   }
 }
