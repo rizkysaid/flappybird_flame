dc0ec7f189f9ae7a1432b8ffd2ffb85c6d931a3c
rizkysaid <rizkysaid40@gmail.com>
2024-01-03 22:55:48 +0700

    overlays

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,17 @@
 import 'package:flame/game.dart';
 import 'package:flappy_bird/game/flappy_bird_game.dart';
+import 'package:flappy_bird/screens/main_menu_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   final game = FlappyBirdGame();
-  runApp(GameWidget(game: game));
+  runApp(
+    GameWidget(
+      game: game,
+      initialActiveOverlays: const [MainMenuScreen.id],
+      overlayBuilderMap: {
+        'mainMenu': (context, _) => MainMenuScreen(game: game),
+      },
+    ),
+  );
 }
